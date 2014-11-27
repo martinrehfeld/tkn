4171cc964010ee0b52de35909d8f87d67ebe8897
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2014-11-27 14:31:53 +0100

    Make demo_srv a gen_server

--- src/demo_srv.erl
+++ src/demo_srv.erl
@@ -1,33 +1,64 @@
 -module(demo_srv).
-
+-behaviour(gen_server).
 -include("demo.hrl").
 
 -define(INTERVAL, 2000).
+-define(SERVER, ?MODULE).
+
+-record(state, {count}).
 
 %% ------------------------------------------------
 %% API Function Exports
 %% ------------------------------------------------
 
--export([start_link/0, loop/1]).
+-export([start_link/0]).
+
+%% ------------------------------------------------
+%% gen_server Function Exports
+%% ------------------------------------------------
+
+-export([init/1, handle_call/3, handle_cast/2,
+         handle_info/2, terminate/2,
+         code_change/3]).
 
 %% ------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------
 
 start_link() ->
-    {ok, spawn_link(fun loop/0)}.
+    gen_server:start_link({local, ?SERVER},
+        ?MODULE, [], []).
+
+%% ------------------------------------------------
+%% gen_server Function Definitions
+%% ------------------------------------------------
+
+init([]) ->
+    {ok, _TRef} =
+        timer:send_interval(?INTERVAL, ping),
+    {ok, #state{count = 1}}.
+
+handle_info(ping, #state{count = Count} = State) ->
+    announce(Count),
+    {noreply, State#state{count = Count + 1}};
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
 
 %% ------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------
-
-loop() ->
-    loop(1).
-
-loop(Count) ->
-    timer:sleep(?INTERVAL),
-    announce(Count),
-    ?MODULE:loop(Count + 1).
 
 announce(Count) ->
     ?INFO_MSG(
